57e108f5770a9feb1e1e3182e37432fad1e89842
tatouroberto <tatouroberto@gmail.com>
2023-08-17 10:50:38 +0200

    fix linters

--- main.js
+++ main.js
@@ -3,22 +3,22 @@ let bookshell = [];
 const addbtn = document.querySelector('#addBtn');
 const BookListContainer = document.querySelector('.books-container');
 
-if (bookshell != null) {
-    display ();
+function display() {
+  BookListContainer.innerHTML = '';
+  bookshell.forEach((item) => {
+    BookListContainer.innerHTML += `
+      <div>
+      <div>${item.name}</div>
+      <div>${item.author}</div>
+      <button class='btn-remove' id='${item.id}' onclick='removeBook(${item.id})'>Remove</button>
+      </div>
+      <hr>
+      `;
+  });
 }
 
-function display () {
-    BookListContainer.innerHTML = "";
-    bookshell.forEach((item) => {
-    BookListContainer.innerHTML += `
-    <div>
-    <div>${item.name}</div>
-    <div>${item.author}</div>
-    <button class='btn-remove' id='${item.id}' onclick='removeBook(${item.id})'>Remove</button>
-    </div>
-    <hr>
-    `;
-  });
+if (bookshell != null) {
+  display();
 }
 
 function addBook() {
@@ -28,11 +28,14 @@ function addBook() {
     author: document.querySelector('#authur').value,
   };
   bookshell.push(book);
-  display ();
+  display();
 }
 addbtn.addEventListener('click', addBook);
 
 function removeBook(id) {
   bookshell = bookshell.filter((item) => item.id !== id);
-  display ();
+  display();
 }
+
+const linters = document.querySelector('linters');
+linters.addEventListener('click', removeBook);
