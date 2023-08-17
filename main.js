dd2563493348b0388477b3877cac2bcffc18abfb
tatouroberto <tatouroberto@gmail.com>
2023-08-17 13:19:41 +0200

    add storage

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let bookshell = [];
 
 const addbtn = document.querySelector('#addBtn');
 const BookListContainer = document.querySelector('.books-container');
+const localData = JSON.parse(localStorage.getItem('Books'));
 
-function display() {
-  BookListContainer.innerHTML = '';
-  bookshell.forEach((item) => {
+if (localData != null) {
+  localData.forEach((item) => {
     BookListContainer.innerHTML += `
       <div>
       <div>${item.name}</div>
@@ -14,11 +14,27 @@ function display() {
       </div>
       <hr>
       `;
+    const book = {
+      id: item.id,
+      name: item.name,
+      author: item.author,
+    };
+    bookshell.push(book);
   });
 }
 
-if (bookshell != null) {
-  display();
+function display() {
+  BookListContainer.innerHTML = '';
+  bookshell.forEach((item) => {
+    BookListContainer.innerHTML += `
+        <div>
+        <div>${item.name}</div>
+        <div>${item.author}</div>
+        <button class='btn-remove' id='${item.id}' onclick='removeBook(${item.id})'>Remove</button>
+        </div>
+        <hr>
+        `;
+  });
 }
 
 function addBook() {
@@ -28,14 +44,15 @@ function addBook() {
     author: document.querySelector('#authur').value,
   };
   bookshell.push(book);
+  localStorage.setItem('Books', JSON.stringify(bookshell));
   display();
 }
 addbtn.addEventListener('click', addBook);
 
 function removeBook(id) {
   bookshell = bookshell.filter((item) => item.id !== id);
+  localStorage.setItem('Books', JSON.stringify(bookshell));
   display();
 }
-
 const linters = document.querySelector('linters');
 linters.addEventListener('click', removeBook);
